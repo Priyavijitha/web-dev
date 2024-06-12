6cd930520834c6c901111e7e5b08beaa798fb59f
Priyavijitha <priyavijitha.03@gmail.com>
2024-06-12 17:11:48 +0530

    today commit

---
+++ Olir/olir.js
@@ -0,0 +1,10 @@
+function bigImg(x) {
+    x.style.height = "530px";
+    x.style.width = "420px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "500px";
+    x.style.width = "390px";
+  }
+ 
